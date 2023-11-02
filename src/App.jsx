461031c895ccb56835bd0916fa04da375e0cc8f9
Indhu-T-Vas <indhuvas95@gmail.com>
2023-11-02 20:09:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import SignUp from "./assets/Pages/SignUp";
+import Login from "./assets/Pages/Login";
+import { Route, Routes } from "react-router-dom";
+import Home from "./assets/Pages/Home";
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+export default App;
